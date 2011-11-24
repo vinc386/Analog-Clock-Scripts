7e9c605d6b41f987fd3314ce30339bf176de6ee6
Vincent Yang <yusyang686@gmail.com>
2011-11-23 22:11:15 -0500

    everything works

--- js/clock.js
+++ js/clock.js
@@ -2,20 +2,22 @@ var deg = 0;
 function startClock()
 {
 	// get time from machine
-	var NOW = new Date();
-	var h = NOW.getHours();
-	var m = NOW.getMinutes();
-	var s = NOW.getSeconds();
-	// update seconds
+	var now = new Date();
+	var h = now.getHours();
+	var m = now.getMinutes();
+	var s = now.getSeconds();
+	// update all positions every second
 	update_sec(s);
-	if (s == 0){
-		// update position of minute arrow while the second arrow hits "12"
-		update_min(m);
-	}
-	if (m == 0){
-		// update position of hour arrow while the minite arrow hits "12"
-		update_hour(h);
-	}
+	update_min(m);
+	update_hour(h);
+	// if (s == 0){
+	// 	// update position of minute arrow while the second arrow hits "12"
+	// 	update_min(m);
+	// }
+	// if (m == 0){
+	// 	// update position of hour arrow while the minite arrow hits "12"
+	// 	update_hour(h);
+	// }
 	// update time every half of a sec
 	t = setTimeout('startClock()', 500)
 }
@@ -28,19 +30,28 @@ function update_sec(sec){
 	// calculate the angle
 	calculate_degree(sec);
 	// rotate the arrow by the degree that is calculated using the "tick" method
-	
+	tick(deg, 'ArrowSec');
 }
 
 function update_min(min){
 	calculate_degree(min);
+	tick(deg, 'ArrowMin');
 }
 
 function update_hour(hr){
+	if (hr > 12){
+		hr -= 12;
+	}
+	hr *= 5; 
 	calculate_degree(hr);
+	tick(deg, 'ArrowHour');
 }
 
 function tick(deg, elmt){
-	// 
-	document.getElementById(elmt).style = "";
+	document.getElementById(elmt).setAttribute("style", "transform:rotate(" + deg + "deg);"
+										+ "-moz-transform: (" + deg + "deg);"
+										+ "-o-transform: (" + deg + "deg);"
+										+ "-webkit-transform:rotate(" + deg + "deg)");
 }
+
 startClock();
